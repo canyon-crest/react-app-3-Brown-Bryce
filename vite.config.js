878ec492202d230d55c9274abd8743202527aa63
3-Brown-Bryce <brownb8673@my.sduhsd.net>
2026-03-11 13:50:26 -0700

    JDIJSD

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  base: 'react-app-3-Brown-Bryce', //Replace with your GitHub repo name
+});
